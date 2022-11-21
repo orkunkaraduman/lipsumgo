f794dbba39f1f6c3f7dd233f14369aec0d95ab60
Orkun Karaduman <orkunkaraduman@gmail.com>
2022-11-21 12:24:28 +0300

    implemented lipsum package

--- pkg/lipsum/lipsum.go
+++ pkg/lipsum/lipsum.go
@@ -1,7 +1,13 @@
-package main
+package lipsum
 
-var lipsum = `
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae purus eu massa fringilla ultricies. Aliquam augue nisl, efficitur eu tellus eget, volutpat varius purus. Pellentesque consectetur eleifend elit vel gravida. Duis mollis sit amet tortor sit amet dignissim. Nam velit lorem, lobortis eget vestibulum et, posuere eu arcu. Integer turpis magna, mattis sed dolor vitae, pretium scelerisque nibh. Vivamus porta pellentesque metus, a congue massa egestas nec. Etiam id ligula laoreet, imperdiet augue sed, scelerisque purus. Donec cursus odio ante, eu venenatis nisl tincidunt in. Praesent vitae lorem accumsan, fringilla orci eu, vehicula odio. Aenean ac odio varius, tincidunt odio eget, tincidunt odio. In nec dolor venenatis, vulputate neque sed, faucibus neque.
+import (
+	"bytes"
+	"io"
+	"strings"
+	"sync"
+)
+
+const Lipsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae purus eu massa fringilla ultricies. Aliquam augue nisl, efficitur eu tellus eget, volutpat varius purus. Pellentesque consectetur eleifend elit vel gravida. Duis mollis sit amet tortor sit amet dignissim. Nam velit lorem, lobortis eget vestibulum et, posuere eu arcu. Integer turpis magna, mattis sed dolor vitae, pretium scelerisque nibh. Vivamus porta pellentesque metus, a congue massa egestas nec. Etiam id ligula laoreet, imperdiet augue sed, scelerisque purus. Donec cursus odio ante, eu venenatis nisl tincidunt in. Praesent vitae lorem accumsan, fringilla orci eu, vehicula odio. Aenean ac odio varius, tincidunt odio eget, tincidunt odio. In nec dolor venenatis, vulputate neque sed, faucibus neque.
 
 Duis sed ex sit amet magna efficitur tristique in at erat. Praesent in elit magna. Donec rhoncus arcu est, non suscipit orci facilisis quis. Etiam ipsum ex, ultrices sed eros eget, molestie sagittis neque. Nam dapibus turpis at elit luctus porta. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent ultricies lacus eget neque volutpat tincidunt.
 
@@ -37,4 +43,29 @@ Fusce pulvinar urna ut eleifend aliquam. Curabitur nec lacus efficitur, ullamcor
 
 Proin leo orci, placerat quis eleifend quis, blandit a est. Vestibulum eu libero a mi sagittis porttitor quis in tortor. Maecenas ut ullamcorper neque. Duis in consectetur tortor, quis euismod felis. Cras eu mi in risus pulvinar ultricies. Sed in lorem lacus. Pellentesque at commodo felis. Maecenas vel tincidunt nulla. Quisque aliquam vel nulla ac tempor. Sed pretium eu purus a elementum. Vivamus ut ullamcorper est.
 
-Praesent in commodo ante, eget lacinia ipsum. Sed luctus pharetra sagittis. Proin sed sapien vel lacus semper venenatis eu id quam. Praesent interdum luctus metus sit amet pulvinar. Etiam turpis odio, dapibus vel erat elementum, aliquam auctor nulla. Phasellus pretium est massa, vitae mattis dui commodo at. Quisque dictum sit amet nulla quis condimentum. Quisque varius eros in nibh vulputate placerat. Proin vitae tempor massa. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas pellentesque odio vitae diam luctus mollis. Cras auctor risus vitae nisi ultricies blandit. Nullam condimentum lorem ut erat suscipit viverra. Nunc tincidunt nisl amet.  `
+Praesent in commodo ante, eget lacinia ipsum. Sed luctus pharetra sagittis. Proin sed sapien vel lacus semper venenatis eu id quam. Praesent interdum luctus metus sit amet pulvinar. Etiam turpis odio, dapibus vel erat elementum, aliquam auctor nulla. Phasellus pretium est massa, vitae mattis dui commodo at. Quisque dictum sit amet nulla quis condimentum. Quisque varius eros in nibh vulputate placerat. Proin vitae tempor massa. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas pellentesque odio vitae diam luctus mollis. Cras auctor risus vitae nisi ultricies blandit. Nullam condimentum lorem ut erat suscipit viverra. Nunc tincidunt nisl amet.`
+
+var sentenceMu sync.Mutex
+var sentenceBuf = bytes.NewBuffer(nil)
+var sentenceIdx int
+
+func GetSentence() (sentence string, index int) {
+	sentenceMu.Lock()
+	defer sentenceMu.Unlock()
+	for {
+		var err error
+		sentence, err = sentenceBuf.ReadString('.')
+		if err != nil {
+			if err == io.EOF {
+				sentenceBuf = bytes.NewBufferString(Lipsum)
+				sentenceIdx = 0
+				continue
+			}
+			panic(err)
+		}
+		index = sentenceIdx
+		sentenceIdx++
+		break
+	}
+	return strings.TrimSpace(sentence), index
+}
