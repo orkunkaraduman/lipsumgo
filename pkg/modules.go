29b36fe6b51f0779de347724e70c5b018102eb12
Orkun Karaduman <orkunkaraduman@gmail.com>
2022-11-21 12:25:00 +0300

    implemented modules.go

---
+++ pkg/modules.go
@@ -0,0 +1,12 @@
+//go:build modules
+// +build modules
+
+package pkg
+
+import (
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+	_ "github.com/grpc-ecosystem/grpc-health-probe"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+)
